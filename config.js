d27405dce51065b65656be6d7ea0565b401a5b03
Shehan Atuk <shehanatukorala@Shehans-MacBook-Pro.local>
2019-07-10 10:42:16 -0400

    first commit, api credentials stored in heroku config vars

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  consumer_key: process.env.CONSUMERKEY,
+  consumer_secret: process.env.CONSUMERSECRET,
+  access_token: process.env.ACCESSTOKEN,
+  access_token_secret: process.env.ACCESSTOKENSECRET,
+  timeout_ms: 60 * 1000 //optional HTTP request timeout to apply to all requests
+};
